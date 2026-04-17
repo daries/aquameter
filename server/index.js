ee915f881bea8abfc7238cedba33762754f48433
root <darysp@gmail.com>
2026-04-17 08:25:43 +0100

    update dari server

--- server/index.js
+++ server/index.js
@@ -1232,6 +1232,12 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'))
   })
+}else{
+  app.use(express.static(path.join(__dirname, '../dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
+  })
+    
 }
 
 // ─── Health check ───
